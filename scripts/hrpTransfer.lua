e2d638e2bb6a9b2790eeb3d6f86799504f5d37f2
skyr3x <43004098+DatSkyr3x@users.noreply.github.com>
2023-08-04 23:04:31 +0200

    Update hrpTransfer.lua

--- scripts/hrpTransfer.lua
+++ scripts/hrpTransfer.lua
@@ -145,6 +145,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(obj,proc)
 		local hit = lp:GetMouse().Hit
 		local tempPart = spawnPart(hit.Position - Vector3.new(0,1,0))
 		placePart(hit.Position + Vector3.new(0,0.5,0))
+		tempPart:Destroy()
 	end
 end)
 
